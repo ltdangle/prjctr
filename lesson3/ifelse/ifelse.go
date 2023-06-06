80721ab3ffd29c4a3435bf4c7016e39a7e33b698
Evgeniy Fedorov <mailbox@z01d.net>
2023-06-06 20:01:16 +0300

    switch statement

---
+++ lesson3/ifelse/ifelse.go
@@ -0,0 +1,8 @@
+package ifelse
+
+func IsABigger(a int, b int) bool {
+	return false
+}
+
+var A = 1
+var b = 2
---
+++ lesson3/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	// "ifelse/ifelse"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// shouldPrint := true
+	// if comparisonResult := ifelse.IsABigger(3, 4); comparisonResult && shouldPrint {
+	// 	fmt.Println("branch one")
+	// } else {
+	// 	fmt.Println("branch two")
+	// }
+
+	rand.Seed(time.Now().UnixNano())
+	switch i := rand.Intn(10); i % 2 {
+	case 0:
+		fmt.Println(i, 0)
+	case 1:
+		fmt.Println(i, 1)
+	}
+}
