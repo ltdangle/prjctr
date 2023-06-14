d254ad364ec84d91069cdb2b2a3d2a29a9b701a8
Evgeniy Fedorov <mailbox@z01d.net>
2023-06-14 15:40:23 +0300

    Grid header

--- hw5/gameLoop.go
+++ hw5/gameLoop.go
@@ -4,12 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	// "strconv"
 )
 
 type gameLoop struct {
-	game    *game
-	player1 *player
-	player2 *player
+	game     *game
+	player1  *player
+	player2  *player
+	nextTurn *player
 }
 
 func NewGameLoop(g *game) *gameLoop {
@@ -18,24 +21,46 @@ func NewGameLoop(g *game) *gameLoop {
 
 func (l *gameLoop) run() {
 	l.choosePlayer1Side()
-	// l.clearScreen()
-	l.drawGrid()
+	l.makeMoves()
 }
+
+func (l *gameLoop) makeMoves() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for l.game.hasEmptyCells() {
+		l.drawGrid()
+		fmt.Print("Player " + l.nextTurn.name + " choose your cell (row, col): ")
+		scanner.Scan()
+		fmt.Println("Player " + l.nextTurn.name + " chose " + scanner.Text())
+	}
+}
+
+func (l *gameLoop) gridHeader() string {
+	var header string = "    "
+	for i := range l.game.grid[0] {
+		header += fmt.Sprintf(" %d  ", i)
+	}
+
+	return header
+}
+
 func (l *gameLoop) drawGrid() {
-	for _, row := range l.game.grid {
+	fmt.Println(l.gridHeader())
+	for rowIndex, row := range l.game.grid {
+		fmt.Printf(" %s |", strconv.Itoa(rowIndex))
 		for _, cell := range row {
 			switch cell.value {
 			case playerX.value:
-				fmt.Print(playerX.name + " |")
+				fmt.Printf(" %s |", playerX.name)
 			case playerO.value:
-				fmt.Print(playerO.name + " |")
+				fmt.Printf(" %s |", playerO.name)
 			default:
-				fmt.Print("  |")
+				fmt.Printf(" %s |", " ")
 			}
 		}
 		fmt.Println()
 	}
 }
+
 func (l *gameLoop) choosePlayer1Side() {
 	scanner := bufio.NewScanner(os.Stdin)
 name:
@@ -56,6 +81,7 @@ name:
 			continue
 		}
 	}
+	l.nextTurn = l.player1
 }
 
 func (l *gameLoop) clearScreen() {
--- hw5/main.go
+++ hw5/main.go
@@ -8,6 +8,8 @@ func main() {
 	game := NewGame()
 	game.SetO(0, 0)
 	game.SetX(1, 0)
+	game.SetX(1, 1)
+	game.SetX(1, 2)
 	loop := NewGameLoop(game)
 	loop.run()
 	os.Exit(0)
