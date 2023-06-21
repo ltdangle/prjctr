609e2797795c578564ee90a84a047d1739f940e5
Evgeniy Fedorov <mailbox@z01d.net>
2023-06-21 13:32:47 +0300

    main3.go

---
+++ lesson7/main3.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// goroutine function
+func addSuffix(c chan string, suffix string) {
+	val := <-c
+	val += suffix
+	c <- val
+}
+
+func main() {
+	ch := make(chan string, 1) // Buffered channel
+
+	// send data to channel
+	ch <- "value1"
+
+	// start goroutine to process channel values
+	go addSuffix(ch, "-suffix1")
+	go addSuffix(ch, "-suffix2")
+
+	// give the goroutine time to process
+	time.Sleep(time.Second)
+
+	// receive data from channel
+	fmt.Println(<-ch)
+
+	// close the channel
+	close(ch)
+}
