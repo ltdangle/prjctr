41735d34711fbc4f7212dd5aafe75e2fc1f02f08
Evgeniy Fedorov <mailbox@z01d.net>
2023-06-21 16:03:33 +0300

    WaitGroup and comments

--- lesson7/main2.go
+++ lesson7/main2.go
@@ -7,9 +7,12 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-
 	myChan := make(chan int)
 
+	// Wait for 1 gorutine to finish
+	wg.Add(1)
+
+	// First, init channel read via gorutine, otherwise it will block.
 	go func() {
 		defer wg.Done()
 		v1 := <-myChan
@@ -19,9 +22,11 @@ func main() {
 		fmt.Println(v1, v2, v3)
 	}()
 
+	// Second, write to channel.
 	myChan <- 1
 	myChan <- 2
 	myChan <- 3
 
+	// Block until our 1 gorutine is finished.
 	wg.Wait()
 }
