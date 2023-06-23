dd644a074dbe25e9a4f892df0c95a53f991a8fcd
Evgeniy Fedorov <mailbox@z01d.net>
2023-06-23 13:10:39 +0300

    Stdin to 2 pipes

--- lesson7/stdin-to-pipe.go
+++ lesson7/stdin-to-pipe.go
@@ -4,20 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	pipe := make(chan string)
+	pipe1 := make(chan string)
+	pipe2 := make(chan string)
 	go func() {
 		counter := 0
-		for text := range pipe {
-			fmt.Printf("%d: %s\n", counter, text)
+		for text := range pipe1 {
+			// fmt.Printf("Pipe1 %d: %s\n", counter, text)
+			pipe2 <- text + strconv.Itoa(counter)
+			counter++
 		}
 	}()
 
+	go func() {
+		counter := 0
+		for text := range pipe2 {
+			fmt.Printf("Pipe2 %d: %s\n", counter, text)
+			counter++
+		}
+	}()
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		pipe <- scanner.Text()
+		pipe1 <- scanner.Text()
 	}
-
 }
