10f69ed05861aa8b0dd8b6d818d07100f522636c
Evgeniy Fedorov <mailbox@z01d.net>
2023-06-27 12:46:43 +0300

    Generate orders in the main loop

--- hw8/shop.go
+++ hw8/shop.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"math/rand"
+	"strconv"
 	"sync"
+	"time"
 )
 
 type product struct {
@@ -18,10 +21,27 @@ func main() {
 	orders := make(chan order)
 	var wg sync.WaitGroup
 	ctx, _ := context.WithCancel(context.Background())
-	wg.Add(1)
-	go OrderGenerator(ctx, orders, &wg)
-	wg.Add(1)
+
 	go OrderTotalCalculator(ctx, orders, &wg)
 
+	for i := 0; i <= 4; i++ {
+		orders <- randomOrder()
+		time.Sleep(1 * time.Second)
+	}
+	close(orders)
+
 	wg.Wait()
+}
+
+// Generates random order.
+func randomOrder() order {
+	// Generate products.
+	var products []product
+	for i := 0; i < rand.Intn(5); i++ {
+		products = append(products, product{name: "product " + strconv.Itoa(rand.Intn(100)), price: rand.Intn(100)})
+	}
+	return order{
+		customer: "Customer " + strconv.Itoa(rand.Intn(100)),
+		products: products,
+	}
 }
