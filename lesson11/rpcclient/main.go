f8a2184555660bc37193e502b02658138e8b9a5b
Evgeniy Fedorov <mailbox@z01d.net>
2023-07-04 20:44:25 +0300

    Lesson11.2

---
+++ lesson11/rpcclient/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/rpc/jsonrpc"
+
+	"github.com/rs/zerolog/log"
+)
+
+type Args struct {
+	A, B int
+}
+
+func main() {
+	client, err := jsonrpc.Dial("tcp", "localhost:8080")
+	if err != nil {
+		panic(err.Error())
+	}
+	args := Args{1, 2}
+	var result int
+	if err := client.Call("Calculator.Add", args, &result); err != nil {
+		log.Fatal().Err(err).Msg("Can't call calculator method")
+	}
+	log.Info().Msgf("%v + %v = %v", args.A, args.B, result)
+
+	if err := client.Call("Calculator.Subtract", args, &result); err != nil {
+		log.Fatal().Err(err).Msg("Can't call calculator method")
+	}
+	log.Info().Msgf("%v + %v = %v", args.A, args.B, result)
+}
