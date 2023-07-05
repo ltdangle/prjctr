6a6c21769e013dad9a121b0929a4fec36366e28f
Evgeniy Fedorov <mailbox@z01d.net>
2023-07-05 17:52:14 +0300

    Round struct.

--- hw8/game.go
+++ hw8/game.go
@@ -5,17 +5,19 @@ import (
 	"time"
 	// "time"
 )
-
+type round struct {
+	id int
+}
 type player struct {
 	playerNumber int
-	gameCh       chan interface{}
+	gameCh       chan round
 }
 
 func main() {
 	var players []*player
 	// Init players.
 	for i := 0; i < 5; i++ {
-		p := &player{playerNumber: i, gameCh: make(chan interface{})}
+		p := &player{playerNumber: i, gameCh: make(chan round)}
 		players = append(players, p)
 		go playerGoroutine(p)
 	}
@@ -26,13 +28,16 @@ func main() {
 func gameLoop(players []*player) {
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
+
+	roundCount:=0
 	for {
 		select {
 		case t := <-ticker.C:
 			fmt.Printf("\nNew round at: %v", t)
 			for _, player := range players {
-				player.gameCh <- ""
+				player.gameCh <-round{id:roundCount} 
 			}
+			roundCount++
 		}
 	}
 }
@@ -41,8 +46,8 @@ func playerGoroutine(p *player) {
 	fmt.Printf("\nPlayer %d is ready.", p.playerNumber)
 	for {
 		select {
-		case <-p.gameCh:
-			fmt.Printf("\nPlayer %d received new round.", p.playerNumber)
+		case round:=<-p.gameCh:
+			fmt.Printf("\nPlayer %d received new round number %d.", p.playerNumber, round.id)
 		}
 	}
 }
