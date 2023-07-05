cfd8806505ec678d2bfa847889d4c7e06673b72a
Evgeniy Fedorov <mailbox@z01d.net>
2023-07-05 18:10:11 +0300

    roundReferee receives integers from players

--- hw8/game.go
+++ hw8/game.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 	// "time"
 )
+
 type round struct {
 	id int
 }
@@ -15,39 +17,53 @@ type player struct {
 
 func main() {
 	var players []*player
+	guesses:=make(chan int)
 	// Init players.
 	for i := 0; i < 5; i++ {
 		p := &player{playerNumber: i, gameCh: make(chan round)}
 		players = append(players, p)
-		go playerGoroutine(p)
+		go playerGoroutine(p, guesses)
 	}
-	go gameLoop(players)
-	time.Sleep(9 * time.Second)
+	go roundGenerator(players)
+	go roundReferee(players,guesses)
+	time.Sleep(90 * time.Second)
+}
+func roundReferee(players []*player, guesses chan int) {
+	for {
+		select {
+		case guess := <-guesses:
+			fmt.Printf("\nroundReferee received: %d", guess)
+		}
+	}
 }
 
-func gameLoop(players []*player) {
-	ticker := time.NewTicker(1 * time.Second)
+func roundGenerator(players []*player) {
+	ticker := time.NewTicker(10 * time.Second)
 	defer ticker.Stop()
 
-	roundCount:=0
+	roundCount := 0
 	for {
 		select {
 		case t := <-ticker.C:
 			fmt.Printf("\nNew round at: %v", t)
 			for _, player := range players {
-				player.gameCh <-round{id:roundCount} 
+				player.gameCh <- round{id: roundCount}
 			}
 			roundCount++
 		}
 	}
 }
 
-func playerGoroutine(p *player) {
+func playerGoroutine(p *player, guesses chan int) {
 	fmt.Printf("\nPlayer %d is ready.", p.playerNumber)
 	for {
 		select {
-		case round:=<-p.gameCh:
+		case round := <-p.gameCh:
 			fmt.Printf("\nPlayer %d received new round number %d.", p.playerNumber, round.id)
+			guess:=rand.Intn(5)
+			time.Sleep(time.Duration(guess)*time.Second)
+			guesses<-guess
+			fmt.Printf("\nPlayer %d for round number %d sent guess %d", p.playerNumber, round.id, guess)
 		}
 	}
 }
