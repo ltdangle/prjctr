9a1209bb8f0e62522448fe0b94abafb426fbe0ad
Evgeniy Fedorov <mailbox@z01d.net>
2023-07-07 19:39:59 +0300

    Added flag cli option.

--- hw9/snippetbox/cmd/web/main.go
+++ hw9/snippetbox/cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	// Handle static files
@@ -15,7 +19,8 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
