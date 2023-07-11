fd8f011920d12db642e4822f6d9c6b54df4d993e
Evgeniy Fedorov <mailbox@z01d.net>
2023-07-11 14:54:40 +0300

    wip

--- hw10/translator.go
+++ hw10/translator.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	u "net/url"
 )
 
 type TranslateResponse struct {
@@ -12,11 +15,25 @@ type TranslateResponse struct {
 	To   string `json:"to"`
 }
 
+// https://translate.googleapis.com/translate_a/single?client=gtx&sl=eng&tl=ru&dt=t&q=hi
 func translateHandler(w http.ResponseWriter, r *http.Request) {
 	// Prepare parameters.
 	text := r.URL.Query().Get("text")
 	from := r.URL.Query().Get("from")
 	to := r.URL.Query().Get("to")
+
+	// Call api.
+	var apiResponse string
+	url := "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + from + "&tl=" + to + "&dt=t&q=" + u.QueryEscape(text)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		respByte, _ := io.ReadAll(resp.Body)
+		apiResponse = string(respByte)
+	}
+
+	fmt.Println(apiResponse)
 
 	translate := TranslateResponse{
 		Text: text,
@@ -31,4 +48,3 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
