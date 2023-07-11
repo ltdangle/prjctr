17e7c466ac701a1a4b1a0ba1268e8caf0965d853
Evgeniy Fedorov <mailbox@z01d.net>
2023-07-11 13:09:44 +0300

    weather ready

--- hw10/weather.go
+++ hw10/weather.go
@@ -1,64 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
 type WeatherResponse struct {
-	Main struct {
-		Temp     float64 `json:"temp"`
-		Pressure int     `json:"pressure"`
-		Humidity int     `json:"humidity"`
-	} `json:"main"`
-	Wind struct {
-		Speed float64 `json:"speed"`
-	} `json:"wind"`
+	City     string  `json:"city"`
+	Temp     float64 `json:"temp"`
+	Wind     float64 `json:"wind"`
+	Humidity float64 `json:"humidity"`
 }
 
 func weatherHandler(w http.ResponseWriter, r *http.Request) {
+	// Prepare parameters.
 	apiKey := os.Getenv("WEATHER_API_KEY")
-	fmt.Println("WEATHER_API_KEY", apiKey)
-
 	city := r.URL.Query().Get("city")
-	url := fmt.Sprintf("https://api.weatherapi.com/v1/forecast.json?key=%s&q=%s&days=1", apiKey, city)
-	fmt.Println("Api url: " + url)
 
+	// Call api.
+	var apiResponse string
+	url := fmt.Sprintf("https://api.weatherapi.com/v1/forecast.json?key=%s&q=%s&days=1", apiKey, city)
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
-		data, _ := ioutil.ReadAll(resp.Body)
-		fmt.Println(string(data))
+		respByte, _ := io.ReadAll(resp.Body)
+		apiResponse = string(respByte)
 	}
-	return
-	// resp, err := http.Get(url)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
-	// defer resp.Body.Close()
-	//
-	// body, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
-	//
-	// fmt.Println(body)
-	//
-	// var weatherResponse WeatherResponse
-	// err = json.Unmarshal(body, &weatherResponse)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
-	// fmt.Println(weatherResponse)
-	//
-	// json.NewEncoder(w).Encode(weatherResponse)
+
+	var result map[string]interface{}
+	err = json.Unmarshal([]byte(apiResponse), &result)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	// Convert api result into our json response.
+	var jsonResponse = WeatherResponse{
+		Temp:     result["current"].(map[string]interface{})["temp_c"].(float64),
+		Wind:     result["current"].(map[string]interface{})["wind_kph"].(float64),
+		Humidity: result["current"].(map[string]interface{})["humidity"].(float64),
+	}
+
+	json.NewEncoder(w).Encode(jsonResponse)
 }
 
 func main() {
