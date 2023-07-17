aa80b190189d467f98aa36e050631338c3cc4918
Evgeniy Fedorov <mailbox@z01d.net>
2023-07-17 21:47:08 +0300

    wip

--- hw12/main.go
+++ hw12/main.go
@@ -1,7 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 
   e:=NewEditor()
   e.AddFormatter(NewNoSpacesFormatter(NewUpperCaseWordsFormatter(NewText("some text here"))))
+  fmt.Println(e.Menu())
 }
