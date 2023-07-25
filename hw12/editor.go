fe9d88b0d34cd9418bf0f8ebaf19e7c4b28651b1
Evgeniy Fedorov <mailbox@z01d.net>
2023-07-25 15:52:02 +0300

    Can combine formatters via input.

--- hw12/editor.go
+++ hw12/editor.go
@@ -5,23 +5,41 @@ import "fmt"
 type Editor struct {
 	Text       Text
 	Stats      map[string]TextStat
-	Formatters map[string]Formatter
+	Formatters map[string]func(t Formatter) Formatter
 }
 
 func NewEditor(text Text) *Editor {
-	return &Editor{
+	e := Editor{
 		Text:       text,
 		Stats:      make(map[string]TextStat),
-		Formatters: make(map[string]Formatter),
+		Formatters: make(map[string]func(t Formatter) Formatter),
 	}
+
+	// Init available formatters.
+	e.Formatters = map[string]func(t Formatter) Formatter{
+		"no-spaces": func(t Formatter) Formatter {
+			return NewNoSpacesFormatter(t)
+		},
+		"upper": func(t Formatter) Formatter {
+			return NewUpperCaseWordsFormatter(t)
+		},
+		"lower": func(t Formatter) Formatter {
+			return NewLowerCaseWordsFormatter(t)
+		},
+	}
+	return &e
+}
+
+// Returns format decorator chain.
+func (e *Editor) Format(formatterKeys []string) Formatter {
+	var f Formatter = e.Text
+	for _, formatter := range formatterKeys {
+		f = e.Formatters[formatter](f)
+	}
+	return f
 }
 
 func (e *Editor) AddStatAction(s TextStat) {
 	key := fmt.Sprintf("s%d", len(e.Stats)+1)
 	e.Stats[key] = s
-}
-
-func (e *Editor) AddFormatter(f Formatter) {
-	key := fmt.Sprintf("f%d", len(e.Formatters)+1)
-	e.Formatters[key] = f
 }
--- hw12/main.go
+++ hw12/main.go
@@ -6,13 +6,14 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 const statKey = "stat"
 const formatKey = "format"
 
 // Receives text via stdin. Formats text and shows text statistics according cli flags.
-// Example usage: `echo "your text here" | go run . -stat=s2 -format=f2`
+// Example usage: `echo "your text here" | go run . -stat=s2 -format=lower,no-spaces,upper`
 // Help: `echo "your text here" | go run . -help`
 func main() {
 	var text string
@@ -32,20 +33,18 @@ func main() {
 	e := NewEditor(t)
 	e.AddStatAction(NewWhiteSpaceCounter(t))
 	e.AddStatAction(NewWhiteSpaceCounterRegex(t))
-	e.AddFormatter(NewNoSpacesFormatter(NewUpperCaseWordsFormatter(t)))
-	e.AddFormatter(NewUpperCaseWordsFormatter(NewNoSpacesFormatter(t)))
-	e.AddFormatter(NewLowerCaseWordsFormatter(t))
 
 	// Parse cli flags.
 	statAction := flag.String(statKey, "", StatHelp(e))
 	formatAction := flag.String(formatKey, "", FormatterHelp(e))
 	flag.Parse()
 
-	// Run action according to cli flags.
-	if *formatAction != "" {
-		format := e.Formatters[*formatAction]
-		fmt.Printf("\n%s:\n%s", format.Description(), format.Format())
-	}
+	// Formatters.
+	formatterKeys := strings.Split(*formatAction, ",")
+	var format Formatter = e.Format(formatterKeys)
+	fmt.Printf("\n%s:\n%s", format.Description(), format.Format())
+
+	// Text stats.
 	if *statAction != "" {
 		stat := e.Stats[*statAction]
 		fmt.Printf("\n%s: %d\n", stat.Description(), stat.Count())
@@ -55,8 +54,8 @@ func main() {
 // Cli help entry for formatters.
 func FormatterHelp(e *Editor) string {
 	var formatters string
-	for key, formatter := range e.Formatters {
-		formatters += fmt.Sprintf("-%s=%s -  %s\n", formatKey, key, formatter.Description())
+	for key, _ := range e.Formatters {
+		formatters += fmt.Sprintf("-%s=%s\n", formatKey, key)
 	}
 	return formatters
 }
