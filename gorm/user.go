6a1366b2dd358ebf9cf3dec413a17cfbe2864b55
Evgeniy Fedorov <mailbox@z01d.net>
2023-08-05 08:58:00 +0300

    gorm init

---
+++ gorm/user.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/jinzhu/gorm"
+type CreditCard struct {
+  gorm.Model
+  Number   string
+  UserID   uint
+}
+
+type User struct {
+  gorm.Model
+  Name       string
+  CreditCard CreditCard
+}
+
